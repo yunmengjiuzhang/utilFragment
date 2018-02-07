241c47d783bb9624035a2179e61be895344b221c
xuanyuanxixi <1020301980@qq.com>
2018-02-07 10:05:16 +0800

    fragmentation基类优化

--- utils/src/main/java/wangfei/util/fragmentation/BaseFragmentVH.java
+++ utils/src/main/java/wangfei/util/fragmentation/BaseFragmentVH.java
@@ -2,8 +2,6 @@ package wangfei.util.fragmentation;
 
 import android.view.View;
 
-import me.yokeyword.fragmentation.SupportFragment;
-
 public class BaseFragmentVH {
 
     public BaseFragment mCtx;
